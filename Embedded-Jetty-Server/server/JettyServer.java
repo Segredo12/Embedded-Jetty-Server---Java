7c45d8633a7cc6e2488602c62622d8e0e382ccc7
Segredo12 <35113790+Segredo12@users.noreply.github.com>
2020-12-02 17:52:50 +0000

    JettyServer.java (Update)
    * Server updated;
    * Server now uses ServletHolder with Jersey parameters;
    * Added custom AuthFilter with Basic Authentication;

--- Embedded-Jetty-Server/server/JettyServer.java
+++ Embedded-Jetty-Server/server/JettyServer.java
@@ -1,7 +1,9 @@
-package pt.segredo12.jettyServer;
+package pt.segredo12.jettyServer.server;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 /*
  * @Created: Octávio Marques.
@@ -11,8 +13,15 @@ public class JettyServer extends Server {
 	// Server const:
 	private Server server;
 	
+	// URL Prefix:
+	private final String urlPrefix = "/rest";
+	
 	// Port to use:
 	private int port = 8080;
+	
+	// Init Constants:
+	private final String initPackage = "pt.segredo12.jettyServer";
+	private final String authFilterPath = "pt.segredo12.jettyServer.filter.AuthFilter";
 	
 	// Prepares Server:
 	public JettyServer() {
@@ -20,6 +29,11 @@ public class JettyServer extends Server {
 		this.server = new Server(port);
 		ServletContextHandler context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
 		server.setHandler(context);
+		
+		ServletHolder sh = new ServletHolder(ServletContainer.class);
+		sh.setInitParameter("jersey.config.server.provider.packages", initPackage); // Adds Package Providers;
+		sh.setInitParameter("javax.ws.rs.container.ContainerRequestFilter", authFilterPath); // Adds our Custom AuthFilter;
+		context.addServlet(sh, urlPrefix+"/*");
 	}
 	
 	// Used to start server:
